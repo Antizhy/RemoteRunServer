8638f301f9eefd803edc0429c5592a471b0660fe
antizhy <antizhy@outlook.com>
2021-12-08 14:52:25 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # RemoteRunServer
+
 远程运行程序服务器端
+
+本质是一个websocket服务器
+
+使用 `typescript` 开发
+
+* 客户端连接后如果发送 `/rc` 会被识别为远程执行客户端
+
+* 客户端连接后如果发送 `/client` 会被识别为远程控制客户端
+
+* 远程控制客户端(console.html)可以发送 `/run` 和 `/exit` 分别表示执行和退出
+
+## 使用方法
+
+需要 `node.js 10.14` 及以后版本
+
+安装依赖
+
+```shell
+npm i
+```
+
+编译运行
+
+```shell
+npm run dev
+```
+
+手动编译运行
+
+```shell
+tsc
+node index.js
+```
+
+## console.html
+
+这是一个纯前端制作的控制台，可以连接服务器端，控制执行客户端，有执行和退出两种功能，直接使用浏览器打开即可使用
---
+++ index.ts
@@ -0,0 +1,146 @@
+import WebSocket from 'ws';
+import UUID from 'short-uuid';
+const mPort = 8088;
+const WServer = WebSocket.Server;
+const wss = new WServer({ port: mPort });
+
+interface ConnectionInfo {
+    [key: string]: WebSocket;
+}
+
+const BLACK = 30;
+const RED = 31;
+const GREEN = 32;
+const YELLOW = 33;
+const BLUE = 34;
+const MAGENTA = 35;
+const CYAN = 36;
+const WHITE = 37;
+
+const corlorLog = (text: string, color: number) :void => {
+    /* 
+    text = 'abcdefg{}hijklmn{}'
+        前景色            背景色           颜色
+        ---------------------------------------
+        30                40              黑色
+        31                41              红色
+        32                42              绿色
+        33                43              黃色
+        34                44              蓝色
+        35                45              紫红色
+        36                46              青蓝色
+        37                47              白色
+    */
+    let tmp = text.split('{}');
+    //console.log(tmp);
+    console.log(`[${getTimeStr()}]${tmp[0]}\x1b[${color}m${tmp[1]}\x1b[0m${tmp[2]}`);
+   
+};
+const getTimeStr = (): string => {
+  let date = new Date();
+  let res = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  return res;
+};
+const conPool: ConnectionInfo = {};
+let rc: string[] = [];
+let cl: string[] = [];
+
+console.log(`[${getTimeStr()}][SERVER] launch server on ${mPort}...`);
+let wsend = (mess: string): number => {
+    if (rc.length > 0) {
+        for(let i of rc) {
+            console.log(`[${getTimeStr()}][SERVER] send [${i}] run client  ${mess}`);
+            conPool[i].send(mess);
+        }
+        return 0;
+    }
+    else {
+        corlorLog(`[SERVER] {}error! no remoterun client!{}`, RED);
+        return -1;
+    }
+};
+let wsendcl = (mess: string): number => {
+    if (rc.length > 0) {
+        for(let i of cl) {
+            console.log(`[${getTimeStr()}][SERVER] send [${i}] control client  ${mess}`);
+            conPool[i].send(mess);
+        }
+        return 0;
+    }
+    else {
+        corlorLog(`[SERVER] {}error! no control client!{}`, RED);
+        return -1;
+    }
+};
+wss.on('connection', (ws) => {
+    // 连接
+    const uid = UUID.generate().toString();
+    //let cid = idNo++;
+    conPool[uid] = ws;
+    console.log(`[${getTimeStr()}][SERVER] client ${uid} has connected!`);
+    //console.log('connected...');
+    ws.on('message', (message) => {
+        let mess = message.toString().split(' ');
+        console.log(`[${getTimeStr()}][SERVER] recieve [${uid}]: ${message.toString()}`);
+        if (mess[0] == '/run') {
+            let res = wsend('/run');
+            let tmp;
+            if (res == 0) {
+                tmp = `服务器向 远程执行客户端 发送 /run`;
+            }
+            else {
+                tmp = '发送失败，远程执行客户端 未连接!';
+            }
+            ws.send(tmp);
+        }
+        else if(mess[0] == "/exit")
+        {
+            let res = wsend('/exit');
+            let tmp;
+            if (res == 0) {
+                tmp = `服务器向 远程执行客户端 发送 /run`;
+            }
+            else {
+                tmp = '发送失败，远程执行客户端 未连接!';
+            }
+            ws.send(tmp);
+        }
+        else if (mess[0] == '/rc') {
+            rc.push(uid);
+            corlorLog(`[SERVER] {}has recognised remoterun client [${uid}].{}`, GREEN);
+            ws.send('/ok');
+        }
+        else if(mess[0] == '/client') {
+            cl.push(uid);
+            corlorLog(`[SERVER] {}has recognised control client [${uid}].{}`, CYAN);
+            ws.send('/ok');
+        }
+        else if(mess[0] == '/info')
+        {
+            //console.log(`[SERVER]${message.toString()}`)
+            wsendcl(message.toString().replace('/info ', ''));
+        }
+        else {
+            let tmp = '复读--' + message.toString();
+            ws.send(tmp);
+        }
+    });
+
+    ws.on('close', () => {
+        let index = rc.findIndex((el)=>{return el == uid;});
+        if (index != -1) {
+            corlorLog(`[SERVER] {}remoterun client [${uid}] is offlining...{}`, YELLOW);
+            rc.splice(index, 1);
+        }
+        index = cl.findIndex((el)=>{return el == uid;});
+        if (index != -1) {
+            corlorLog(`[SERVER] {}control client [${uid}] is offlining...{}`, YELLOW);
+            cl.splice(index, 1);
+        }
+        corlorLog(`[SERVER] {}[${uid}] close{}`, MAGENTA);
+        delete conPool[uid];
+    });
+
+});
+
+
